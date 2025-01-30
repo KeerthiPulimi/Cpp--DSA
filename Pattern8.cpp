c4db1250ca9f04b21e5ea9d4b5506602a2f81e18
KeerthiPulimi <pulimikeerthi155@gmail.com>
2025-01-30 23:23:08 +0530

    added patter 8 with reverse triangle

--- Pattern8.cpp
+++ Pattern8.cpp
@@ -5,15 +5,15 @@ void print5(int n)
 {
   for (int i = 0; i < n; i++)
   {
-    for (int j = 0; j < n - i - 1; j++)
+    for (int j = 0; j < i; j++)
     {
       cout << " ";
     }
-    for (int j = 0; j < 2 * i + 1; j++)
+    for (int j = 0; j < 2*n-(2 * i + 1); j++)
     {
-      cout << "*";
+      cout  << "*";
     }
-    for (int j = 0; j < n - i - 1; j++)
+    for (int j = 0; j < i; j++)
     {
       cout << " ";
     }
@@ -33,9 +33,9 @@ int main()
   }
 }
 
-/*     *
-      ***
-     *****
-    *******
-   *********
+/*   *********
+      *******
+       *****
+        ***
+         *
  */
