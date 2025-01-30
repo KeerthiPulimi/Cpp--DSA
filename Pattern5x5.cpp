b9ab806e0ae4eec7bf7da3da1b40f2f8ab52abb8
KeerthiPulimi <pulimikeerthi155@gmail.com>
2025-01-30 13:50:11 +0530

    will be doing patterns anow and the first pattern is 5x5

---
+++ Pattern5x5.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int i, j;
+  for (i = 0; i < 5; i++)
+  {
+    for (j = 0; j < 5; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
